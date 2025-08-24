a77a992799a8cc527f284db8e21f716d99584e78
Trinh Phan Minh Tri <trinhphanminhtri@gmail.com>
2025-08-25 00:57:34 +0700

    third commit - adding domain

--- nanuprinting/settings.py
+++ nanuprinting/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY=os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nanuprintinggroup.com', 'www.nanuprintinggroup.com', '14.225.204.226']
 
 
 # Application definition
