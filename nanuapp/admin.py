3cb47c449eb90248cab2e3dc5f6bdfb9fb2da710
Trinh Phan Minh Tri <trinhphanminhtri@gmail.com>
2025-08-28 14:02:45 +0700

    sql - admin - model

--- nanuapp/admin.py
+++ nanuapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ExampleModel  # Replace ExampleModel with your actual model name
+class ExampleModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')  # Adjust fields as necessary
+    
+admin.site.register(ExampleModel,ExampleModelAdmin)  # Replace ExampleModel with your actual model name
--- nanuprinting/settings.py
+++ nanuprinting/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY=os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['nanuprintinggroup.com', 'www.nanuprintinggroup.com', '14.225.20.226']
+if DEBUG:
+    ALLOWED_HOSTS = []
+else:
+    ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'nanuprinting.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
