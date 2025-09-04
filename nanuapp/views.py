92e26396803a9c1749490310e7fe44b45223d834
Trinh Phan Minh Tri <trinhphanminhtri@gmail.com>
2025-09-04 13:01:41 +0700

    adding active_page - images for portfolio

--- nanuapp/views.py
+++ nanuapp/views.py
@@ -8,25 +8,25 @@ from .forms import ContactForm
 def index(request):
     try:
         video_url = '/media/videos/about_office_staff.mp4'
-        return render(request, 'nanuapp/index.html',{'video_url': video_url})
+        return render(request, 'nanuapp/index.html',{'active_page':'home','video_url': video_url})
     except:
           raise Http404()
 
 def about(request):
     try:
-            return render(request, 'nanuapp/about.html')
+            return render(request, 'nanuapp/about.html',{'active_page':'about'})
     except:
             raise Http404() # if DEBUG=False, this will render 404.html
 
 def portfolio(request):
     try:
-            return render(request, 'nanuapp/portfolio.html')
+            return render(request, 'nanuapp/portfolio.html',{'active_page':'portfolio'})
     except:
             raise Http404()
 
 def blog(request):
     try:
-            return render(request, 'nanuapp/blog.html')
+            return render(request, 'nanuapp/blog.html',{'active_page':'blog'})
     except:
             raise Http404()
 
@@ -36,7 +36,7 @@ def contact(request):
                 form = ContactForm(request.POST)
                 if form.is_valid():
                         name = form.cleaned_data['name']
-                        email = form.cleaned_data['email']
+                        sender_email = form.cleaned_data['email']
                         subject = form.cleaned_data['subject']
                         message = form.cleaned_data['message']
                                 
@@ -44,14 +44,14 @@ def contact(request):
                         send_mail(
                                 f'{subject} {name}',
                                 message,
-                                email,
-                                [settings.EMAIL_HOST_USER],
+                                from_email=sender_email,
+                                recipient_list=[settings.EMAIL_HOST_USER],
                                 fail_silently=False,
                         )
                 return redirect('contact')  # Redirect to a new URL after POST
         else:
-                form = ContactForm()
-        return render(request, 'nanuapp/contact.html', {'form': form})
+              form = ContactForm()
+        return render(request, 'nanuapp/contact.html', {'active_page':'contact','form': form})
 
     except:
         raise Http404()
